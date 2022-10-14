f8b15cf4f134a5a4dec2b6d07d1dc67fb899c25b
Bfunk54 <ben_fein@icloud.com>
2022-10-14 13:57:59 -0400

    added to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var userInput = document.getElementById('userSearch'); 
 var firstCarousel = document.getElementById("carousel")
 var splide = new Splide( firstCarousel, {
     perPage: 3,
@@ -5,3 +6,21 @@ var splide = new Splide( firstCarousel, {
   } );
   
   splide.mount();
+
+
+  const tastySettings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=",
+    "method": "GET",
+    "headers": {
+      "X-RapidAPI-Key": "3dbfc0433dmsh56b1204eaeeb95dp104faejsnc54fdda40931",
+      "X-RapidAPI-Host": "tasty.p.rapidapi.com"
+    }
+  };
+
+  console.log(userInput.value);
+
+  addEventListener('click', function(event){
+
+  })
