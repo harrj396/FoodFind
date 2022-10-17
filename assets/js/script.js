df7e2bded5e4e9bd2b0035beb141d51dfaef2023
Bfunk54 <ben_fein@icloud.com>
2022-10-16 20:58:17 -0400

    FINALLY GOT DATA FROM THE APIONTO THE PAGE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
+const firstRecipeName = document.getElementById('one');
+const input = firstRecipeName || {}
 // Future Functionality
+
 
 // var userInput = document.getElementById('userSearch'); 
 // var firstCarousel = document.getElementById("carousel")
 
 //Defining custom variables
-var mealDBurl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+const mealDBurl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 //Carousel setup
 // var splide = new Splide( firstCarousel, {
@@ -13,11 +16,55 @@ var mealDBurl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 //   } );
   
 //   splide.mount();
+// var recipeName;
+// var getRecipeNameFunction = function(recipe) {
+//   console.log(recipe);
+//   OGrecipeName.document.createElement('p');
+//   OGrecipeName.textContent = recipe;
+//   console.log(OGrecipeName)
+// };
 
-fetch(mealDBurl)
-  .then((response) => response.json())
-  .then((data) => console.log(data.meals[0].strMeal))
-  .then((data) => console.log(data.meals[0].strMeal));
+// fetch(mealDBurl)
+//   .then(response => response.json())
+//   .then(response => {
+//     createRecipe(response.meals[0].strMeal);
+//   });
+
+//   const createRecipe = recipe => {
+//     // var recipeName = document.createElement('p');
+//     // recipeName = [];
+//     // recipeName.textContent = recipe;
+//     // console.log(recipeName); 
+//     document.getElementById('firstRecipeName') = recipe;
+//   }
+  // .then(data => console.log(data.meals[0].strMeal))
+  // .then(data => function(data) {
+  //   console.log(data.meals[0].strMeal);
+  //   var recipeName = document.createElement('p');
+  //   recipeName.textContent = data.meals[0].strMeal;
+  //   console.log(recipeName); 
+  //   OGrecipeName.append(recipeName);
+  // })
+  // .catch(() => {
+  //   console.log("Error");
+  // });
+  // input.value = 'new value'
+
+      async function getRecipe() {
+        const response = await fetch(mealDBurl);
+        const data = await response.json();
+        const newRecipe  = data.meals[0].strMeal;
+        console.log(newRecipe);
+        const firstRecipeName = document.getElementById('one');
+        const input = firstRecipeName || {}
+        input.textContent = newRecipe;
+        console.log(input);
+        // document.getElementById('lon').textContent = longitude.toFixed(2);
+      }
+
+      getRecipe();
+
+
 
 
 
