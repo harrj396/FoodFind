2439e43acf509c1bb73314ba931bd8b78d84f8dc
Bfunk54 <ben_fein@icloud.com>
2022-10-16 21:01:59 -0400

    Cleaned it up and added comme nts

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,47 +9,7 @@ const input = firstRecipeName || {}
 //Defining custom variables
 const mealDBurl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
-//Carousel setup
-// var splide = new Splide( firstCarousel, {
-//     perPage: 3,
-//     rewind : true,
-//   } );
-  
-//   splide.mount();
-// var recipeName;
-// var getRecipeNameFunction = function(recipe) {
-//   console.log(recipe);
-//   OGrecipeName.document.createElement('p');
-//   OGrecipeName.textContent = recipe;
-//   console.log(OGrecipeName)
-// };
-
-// fetch(mealDBurl)
-//   .then(response => response.json())
-//   .then(response => {
-//     createRecipe(response.meals[0].strMeal);
-//   });
-
-//   const createRecipe = recipe => {
-//     // var recipeName = document.createElement('p');
-//     // recipeName = [];
-//     // recipeName.textContent = recipe;
-//     // console.log(recipeName); 
-//     document.getElementById('firstRecipeName') = recipe;
-//   }
-  // .then(data => console.log(data.meals[0].strMeal))
-  // .then(data => function(data) {
-  //   console.log(data.meals[0].strMeal);
-  //   var recipeName = document.createElement('p');
-  //   recipeName.textContent = data.meals[0].strMeal;
-  //   console.log(recipeName); 
-  //   OGrecipeName.append(recipeName);
-  // })
-  // .catch(() => {
-  //   console.log("Error");
-  // });
-  // input.value = 'new value'
-
+//Define async funtion to grab data from api and append it to the page
       async function getRecipe() {
         const response = await fetch(mealDBurl);
         const data = await response.json();
@@ -59,9 +19,9 @@ const mealDBurl = 'https://www.themealdb.com/api/json/v1/1/random.php';
         const input = firstRecipeName || {}
         input.textContent = newRecipe;
         console.log(input);
-        // document.getElementById('lon').textContent = longitude.toFixed(2);
       }
 
+      //Call the function
       getRecipe();
 
 
@@ -69,9 +29,6 @@ const mealDBurl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 
   // fetch('https://low-carb-recipes.p.rapidapi.com/search?name=' + foodName, options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
 
   
   // Future Functionality
